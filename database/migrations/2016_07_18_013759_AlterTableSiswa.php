b39776c63a36a2da74bf3ceb63e1512c20acdf73
brryfrmnn <clickdisini3@gmail.com>
2016-07-19 19:55:37 +0700

    alter table admin dan siswa

---
+++ database/migrations/2016_07_18_013759_AlterTableSiswa.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableSiswa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('siswa', function (Blueprint $table) {
+            //
+             $table->unsignedInteger('admin_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('siswa', function (Blueprint $table) {
+            //
+        });
+    }
+}
